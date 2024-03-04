28375d803697d19ba5831d62026d9ab70bc930e4
Axel Moras <axel.moras78@gmail.com>
2024-03-04 16:44:26 +0100

    jour1

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,35 @@
 import styles from '../styles/Home.module.css';
 
 function Home() {
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.main}>
+
+      <div className={styles.header}>
+        <h1>Header</h1>
+      </div>
+
+      <div className={styles.middleContent}>
+        <h1 className={styles.title}>Quel est votre Projet ?</h1>
+        <button className={styles.achat}>Acheter</button>
+        <button className={styles.vente}>Vendre</button>
+      </div>
+      
+      <div className={styles.LleftContent}>
+        <h3>Qui sommes nous ?</h3>
+        <p>Lorem ipsum</p>
+      </div>
+
+      <div className={styles.LmidContent}>
+        <img src="helmuthd.jpg" className={styles.carrousel}/>
+        <p>Ils nous ont fait confiance</p>
+      </div>
+
+      <div className={styles.LrightContent}>
+        <h3>Pourquoi choisir HomeMatcher ?</h3>
+        <p>Lorem ipsum</p>
+      </div>
+
     </div>
   );
 }
