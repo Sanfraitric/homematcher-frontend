b2403390a41ffeaa65d93a6168d28b41e03f578a
Clément Boulanger <clement.boulangerpro@gmail.com>
2024-03-04 16:44:35 +0100

    branche clément jour 1

---
+++ pages/AddRealtyPage.js
@@ -0,0 +1,7 @@
+import AddRealty from '../components/AddRealty';
+
+function AddRealtyPage() {
+  return <AddRealty />;
+}
+
+export default AddRealtyPage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,25 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import realtys from '../reducers/realtys';
+
+const store = configureStore({
+  reducer: { realtys }
+})
 
 function App({ Component, pageProps }) {
+
+
+
   return (
     <>
+    <Provider store={store}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
