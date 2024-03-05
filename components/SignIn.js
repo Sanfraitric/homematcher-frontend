6c9c665389ce0c9a046c709b83d803c3ea00f480
Mathieu <mathieunr@hotmail.fr>
2024-03-05 08:34:23 +0100

    rajout fonctionnalité de connection et concordance des modals

--- components/SignIn.js
+++ components/SignIn.js
@@ -2,68 +2,76 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../reducers/user.js';
-import Image from 'next/image';
 import styles from '../styles/SignIn.module.css';
-import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
 import SignUp from './SignUp';
 import { Modal } from 'antd';
 
+
 function SignIn() {
+  const BACKEND_URL= process.env.BACKEND_URL
   const dispatch = useDispatch();
+  const router = useRouter();
   const user = useSelector((state) => state.user.value);
-  const [signInModalVisible, setSignInModalVisible] = useState(false); 
-  const [signUpModalVisible, setSignUpModalVisible] = useState(false); 
-
-
-  const handleCancelSignIn = () => {
-      setSignInModalVisible(false);
-  };
+  const [signInModalVisible, setSignInModalVisible] = useState(false);
+  const [signUpModalVisible, setSignUpModalVisible] = useState(false);
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
 
   const showSignUpModal = () => {
-      setSignUpModalVisible(true);
-      setSignInModalVisible(false); 
+    setSignUpModalVisible(true);
+    setSignInModalVisible(false);
   };
 
   const handleCancelSignUp = () => {
-      setSignUpModalVisible(false);
+    setSignUpModalVisible(false);
   };
 
 
-  const router = useRouter();
-  if (user.token) {
-    router.push('/home');
-  }
-
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
 
   const handleSubmit = () => {
-    fetch('*****************************************', {
+    fetch(`${BACKEND_URL}/users/signin`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ mail, password }),
+      body: JSON.stringify({  email: signInEmail,password: signInPassword }),
     }).then(response => response.json())
       .then(data => {
         data.result && dispatch(login({ token: data.token, firstName: data.firstName, username: data.username }));
+        setSignInEmail('');
+        setSignInPassword('');
       });
   };
 
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
 
   return (
     <div className={styles.container}>
+
       <h2 className={styles.title}>Se connecter</h2>
       <div className={styles.toConnect}>
-        <h4>Vous ne possedez pas encore de compte ?</h4>
-        <button onClick={showSignUpModal} className={styles.button}>Créer un compte</button>
+        <h4 className={styles.h4}>Vous n'etes pas encore inscrit ?</h4>
+        <button onClick={showSignUpModal} className={styles.connectButton}>Créer un compte</button>
       </div>
-      <h4>Votre e-mail:</h4>
-      <input type="text" className={styles.input} onChange={(e) => setUsername(e.target.value)} value={mail} />
-      <h4>Votre mot de passe:</h4>
-      <input type="password" className={styles.input} onChange={(e) => setPassword(e.target.value)} value={password} />
+      <h4 className={styles.h4}>Votre e-mail:</h4>
+      <input type="text" className={styles.input} onChange={(e) => setSignInEmail(e.target.value)} value={signInEmail} />
+      <h4 className={styles.h4}>Votre mot de passe:</h4>
+      <div className={styles.connect}>
+      <input type="password" className={styles.input} onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} />
       <button className={styles.button} onClick={() => handleSubmit()}>Se connecter</button>
+      <h4 className={styles.h4}>ou</h4>
+      <GoogleLogin
+        clientId="313442107107-r67n8849np3ndu8sqllj4qblsbd0eh7c.apps.googleusercontent.com"
+        buttonText="Sign In with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      </div>
       <Modal onCancel={handleCancelSignUp} visible={signUpModalVisible} footer={null}>
-                <SignUp />
-            </Modal>
+        <SignUp />
+      </Modal>
     </div>
   );
 }
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,31 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../reducers/user';
 import styles from '../styles/SignUp.module.css';
 import GoogleLogin from 'react-google-login';
+import SignIn from './SignIn';
+import { Modal } from 'antd';
 
 function SignUp() {
-
+  const BACKEND_URL= process.env.BACKEND_URL
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.value);
-
   const router = useRouter();
-  if (user.token) {
-    router.push('/home');
-  }
+  const user = useSelector((state) => state.user.value);
+  const [signInModalVisible, setSignInModalVisible] = useState(false);
+  const [signUpModalVisible, setSignUpModalVisible] = useState(false);
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
 
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
+  const showSignInModal = () => {
+    setSignInModalVisible(true);
+    setSignUpModalVisible(false);
+  };
+
+  const handleCancelSignIn = () => {
+    setSignInModalVisible(false);
+  };
 
   const handleSubmit = () => {
-    fetch('***************************************', {
+    fetch(`${BACKEND_URL}/users/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ mail }),
+      body: JSON.stringify({ email: signUpEmail,password: signUpPassword }),
     }).then(response => response.json())
       .then(data => {
         data.result && dispatch(login({ token: data.token, mail }));
+        setSignUpEmail('');
+        setSignUpPassword('');
       });
   };
 
@@ -36,28 +46,30 @@ function SignUp() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.createaccount}>
+
         <h2 className={styles.title}>Créer un compte</h2>
         <div className={styles.toConnect}>
           <h4 className={styles.h4}>Vous avez déjà un compte ?</h4>
-          <h5 className={styles.h5}>Se connecter</h5>
+          <button className={styles.connectButton} onClick={showSignInModal}>Se connecter</button>
         </div>
-      <h4 className={styles.h4}>Votre e-mail:</h4>
-      <input type="text" className={styles.input} onChange={(e) => setMail(e.target.value)} value={mail} />
-      <h4 className={styles.h4}>Votre mot de passe:</h4>
-      <input type="password" className={styles.input} onChange={(e) => setPassword(e.target.value)} value={password}  />
+        <h4 className={styles.h4}>Votre e-mail:</h4>
+        <input type="text" className={styles.input} onChange={(e) => setSignUpEmail(e.target.value)} value={signUpEmail} />
+        <h4 className={styles.h4}>Votre mot de passe:</h4>
+        <input type="password" className={styles.input} onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
+      <div className={styles.connect}>
+        <button className={styles.button} onClick={() => handleSubmit()}>Créer un compte</button>
+        <h4 className={styles.h4}>ou</h4>
+        <GoogleLogin
+          clientId="313442107107-r67n8849np3ndu8sqllj4qblsbd0eh7c.apps.googleusercontent.com"
+          buttonText="Sign Up with Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </div>
-        <div className={styles.connect}>
-          <button className={styles.button} onClick={() => handleSubmit()}>Créer un compte</button>
-          <h4 className={styles.h4}>ou</h4>
-          <GoogleLogin
-                clientId="313442107107-r67n8849np3ndu8sqllj4qblsbd0eh7c.apps.googleusercontent.com"
-                buttonText="Sign Up with Google"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-            />      
-      </div>
+      <Modal onCancel={handleCancelSignIn} visible={signInModalVisible} footer={null}>
+        <SignIn />
+      </Modal>
     </div>
   );
 }
