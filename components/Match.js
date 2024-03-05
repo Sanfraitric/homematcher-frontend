df8332ac0c808ce2dcf93477e0f865e0e07b5506
Axel Moras <axel.moras78@gmail.com>
2024-03-05 16:13:48 +0100

    jour2

---
+++ components/Match.js
@@ -0,0 +1,24 @@
+import styles from '../styles/Match.module.css';
+import Header from './Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+function Match() {
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.header}>
+                <Header/>
+            </div>
+            <div className={styles.body}>
+                <div className={Match}>
+                    <img src='helmuthd.jpg' className={styles.profilpic}/>
+                    <h3 className={styles.username}>Helmut</h3>
+                    <FontAwesomeIcon icon={faUser} className={styles.icon}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Match
